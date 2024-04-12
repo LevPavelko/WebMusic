0b31a817a1ab2b084ff0bfbbcbaac52b0b67b865
LevPavelko <lev.pavelko.studies@gmail.com>
2024-04-12 10:55:14 +0200

    Task14: Заготовка для локализации сайта

---
+++ WebMusic/Filters/CultureAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebMusic.Filters
+{
+    public class CultureAttribute : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+        }
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string? cultureName = null;
+           
+            var cultureCookie = filterContext.HttpContext.Request.Cookies["lang"];
+            if (cultureCookie != null)
+                cultureName = cultureCookie;
+            else
+                cultureName = "ru";
+
+            // Список культур
+            List<string> cultures = new List<string>() { "ru", "en", "uk", "de" };
+            if (!cultures.Contains(cultureName))
+            {
+                cultureName = "ru";
+            }
+           
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureName);
+            
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(cultureName);
+           
+        }
+    }
+}
